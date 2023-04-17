756ed17a9c4ad440a644a043f595bc60011f1ffb
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2023-04-18 01:52:41 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/App.scss';
+
+import logo from './assets/img/logo.svg';
+import moon from './assets/img/icon-moon.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main">
+      {/* Шапка сайта */}
+      <header className="header">
+        <div className="header-left-block">
+          <img src={logo} alt="logo" />
+        </div>
+        <div>{/* выплывающий список */}</div>
+        <div className="header-right-block">
+          <label className="switch">
+            <input type="checkbox" className="switch-input" />
+            <span className="switch-span"></span>
+          </label>
+          <div className="moon">
+            <img src={moon} alt="logo" className="moon-logo" />
+          </div>
+        </div>
       </header>
+      {/* Поле для ввода */}
+      <div className="input-block">
+        <input className="input" placeholder="Search for any word..." type="text" />
+      </div>
     </div>
   );
 }
